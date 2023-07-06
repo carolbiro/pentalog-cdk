196426f03393fadc35b86922d01d4f68446824c7
carol.biro <carol.biro@code11.com>
2023-07-06 10:11:33 +0300

    Moved constants to external file

---
+++ helper/utils.ts
@@ -0,0 +1,24 @@
+export const CO2_MULTIPLIERS: { [key: string]: number } = {
+    AIRPLANE: 144,
+    BICYCLE: 0,
+    BICYCLE_RENT: 0,
+    BUS: 74,
+    CAR: 160,
+    ESCOOTER: 110,
+    FERRY: 144,
+    FUNICULAR: 54,
+    METRO: 54,
+    PUBLIC_TRANSIT: 74,
+    RAIL: 14,
+    SCOOTER: 75,
+    SHARED_BICYCLE: 0,
+    SUBWAY: 74,
+    TAXI: 250,
+    TRAIN: 14,
+    TRAM: 54,
+    WAIT: 0,
+    WALK: 0,
+  };
+  
+  export const TRIP_PLANNER_BASE_URL = "https://finland-staging.trip-planner.maas.global/otp/routers/default";
+  export const API_KEY = "2TCLEtnmLprXDMVroWbsUu8xyUJaK2MPrkaue7rm";
--- lambda/index.ts
+++ lambda/index.ts
@@ -1,31 +1,11 @@
 import axios from "axios";
 import { APIGatewayEvent } from "aws-lambda";
 import { Plan, Itinerary, Leg } from "../helper/types";
-
-const CO2_MULTIPLIERS: { [key: string]: number } = {
-  AIRPLANE: 144,
-  BICYCLE: 0,
-  BICYCLE_RENT: 0,
-  BUS: 74,
-  CAR: 160,
-  ESCOOTER: 110,
-  FERRY: 144,
-  FUNICULAR: 54,
-  METRO: 54,
-  PUBLIC_TRANSIT: 74,
-  RAIL: 14,
-  SCOOTER: 75,
-  SHARED_BICYCLE: 0,
-  SUBWAY: 74,
-  TAXI: 250,
-  TRAIN: 14,
-  TRAM: 54,
-  WAIT: 0,
-  WALK: 0,
-};
-
-const TRIP_PLANNER_BASE_URL =
-  "https://finland-staging.trip-planner.maas.global/otp/routers/default";
+import {
+  TRIP_PLANNER_BASE_URL,
+  CO2_MULTIPLIERS,
+  API_KEY,
+} from "../helper/utils";
 
 export async function handler(event: APIGatewayEvent) {
   console.log("Event::: ", event);
@@ -48,7 +28,7 @@ export async function handler(event: APIGatewayEvent) {
       cale=en'`,
       {
         headers: {
-          "x-api-key": "2TCLEtnmLprXDMVroWbsUu8xyUJaK2MPrkaue7rm",
+          "x-api-key": API_KEY,
         },
       }
     );
